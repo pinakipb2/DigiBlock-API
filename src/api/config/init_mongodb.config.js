5b84d80c43a6d70832b143798a33cbad1d74223d
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2021-10-23 13:54:44 +0530

    Changed DB name

--- src/api/config/init_mongodb.config.js
+++ src/api/config/init_mongodb.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, {
-  dbName: process.env.ADMIN_DB_NAME,
+  dbName: process.env.DB_NAME,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
