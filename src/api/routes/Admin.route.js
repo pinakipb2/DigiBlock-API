c8cdefbd51fe976e36a4cd77439583635b1860f7
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2021-12-27 13:54:49 +0530

    Added AES 256 enc-dec for masterKey and modified the api, added route for validating masterKey

--- src/api/routes/Admin.route.js
+++ src/api/routes/Admin.route.js
@@ -4,28 +4,63 @@ const path = require('path');
 const genMasterKey = require('../controllers/genMasterKey');
 const Admin = require('../models/admin.model');
 const sendMasterKeyMail = require('../controllers/sendMasterKeyMail');
+const { encrypt, decrypt } = require('../controllers/encryptDecrypt');
 
 router.post('/gen-master-key', async (req, res, next) => {
   try {
     const masterKey = genMasterKey();
-    console.log(req.body);
-    const { name, address, email } = req.body;
-    const replacements = {
-      username: name,
-      masterKey,
-    };
-    const maillist = [email];
-    const subject = 'Master key 🔑';
-    const text = 'Thanks for Registering with DigiBlock.';
-    sendMasterKeyMail(path.join(__dirname, '../../assets/static/masterKey.html'), replacements, maillist, subject, text);
+    // console.log(masterKey);
+    const encMasterKey = encrypt(process.env.MASTER_KEY_SECRET, masterKey);
+    if (encMasterKey.status === 0) {
+      throw encMasterKey.data;
+    }
+    const salt = await bcrypt.genSalt(11);
+    const hashedMasterKey = await bcrypt.hash(masterKey, salt);
+    res.send({ masterKey: encMasterKey.data, hashedMasterKey });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/send-master-key', async (req, res, next) => {
+  try {
+    // console.log(req.body);
+    const {
+      name, address, email, masterKey,
+    } = req.body;
+    const decMasterKey = decrypt(process.env.MASTER_KEY_SECRET, masterKey);
+    // console.log(`Received Master Key : ${decMasterKey.data}`);
+    if (decMasterKey.status === 0) {
+      throw decMasterKey.data;
+    }
+
     const admin = new Admin({
       name,
       address,
     });
     await admin.save();
-    const salt = await bcrypt.genSalt(11);
-    const hashedMasterKey = await bcrypt.hash(masterKey, salt);
-    res.send({ masterKey: hashedMasterKey });
+
+    const text = 'Thank You for Associating as an Admin with DigiBlock.';
+    const replacements = {
+      name,
+      masterKey: decMasterKey.data,
+      text,
+    };
+    const maillist = [email];
+    const subject = 'Master key 🔑';
+    sendMasterKeyMail(path.join(__dirname, '../../assets/static/masterKey.html'), replacements, maillist, subject, text);
+
+    res.status(200).send('OK');
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/validate-master-key', async (req, res, next) => {
+  try {
+    const { masterKey, masterKeyHash } = req.body;
+    const status = await bcrypt.compare(masterKey, masterKeyHash);
+    res.send({ status });
   } catch (err) {
     next(err);
   }
