fb95f5f4fe533fbbd4876f12aa8a1ced18484341
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2022-02-19 21:00:11 +0530

    Added Key Generators

--- src/keys/generateKeys.js
+++ src/keys/generateKeys.js
@@ -5,19 +5,21 @@ const key1 = crypto
   .randomBytes(16)
   .toString('hex')
   .split('')
-  .map((v) => (Math.round(Math.random()) ? v.toUpperCase() : v.toLowerCase()))
+  .map((ch) => (Math.round(Math.random()) ? ch.toUpperCase() : ch.toLowerCase()))
   .join('');
 const key2 = crypto
   .randomBytes(16)
   .toString('hex')
   .split('')
-  .map((v) => (Math.round(Math.random()) ? v.toUpperCase() : v.toLowerCase()))
+  .map((ch) => (Math.round(Math.random()) ? ch.toUpperCase() : ch.toLowerCase()))
   .join('');
+
 // For Secret Keys
 const key3 = crypto
   .randomBytes(32)
   .toString('hex')
   .split('')
-  .map((v) => (Math.round(Math.random()) ? v.toUpperCase() : v.toLowerCase()))
+  .map((ch) => (Math.round(Math.random()) ? ch.toUpperCase() : ch.toLowerCase()))
   .join('');
+
 console.table({ key1, key2, key3 });
